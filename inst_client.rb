ca95395efe4a3ea90c11e3970c41949c9408fb75
Iain Walker <iain.walker@avenir-technology.com>
2018-05-03 15:30:38 +1200

    Remove some inst files

--- inst_client.rb
+++ inst_client.rb
@@ -1,11 +1,6 @@
 require 'rest-client'
 
 fbond   = File.new('./files/Instrument_Bond.xml', 'rb')
-fequity = File.new('./files/Instrument_Equity.xml', 'rb')
-ffuture = File.new('./files/Instrument_Future.xml', 'rb')
-foption = File.new('./files/Instrument_Option.xml', 'rb')
-frepo   = File.new('./files/Instrument_Repo.xml', 'rb')
-fsbl    = File.new('./files/Instrument_SBL.xml', 'rb')
 
 base = 'http://byma.avenir.tech:3000'
 
